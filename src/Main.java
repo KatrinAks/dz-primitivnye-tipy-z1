7646ce259fbe746f4894fad5b768c4e262c2f20a
kat test <e_aksenova80@mail.ru>
2023-03-23 11:43:13 +0300

    Main.java add

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@ public class
 Main {
     public static void main(String[] args) {
 
-        int
+        int ticket = 1645;  // стоимость билета
+        int mile = 20;  // миля
+        int bonus; // количество начиленных миль
+
+        if (ticket > 0) ;
+        {
+            bonus = ticket / mile;
+        }
+        System.out.println("Количество бонусных миль за покупку: " + bonus);
     }
 }
