653d9c8410f60c95093af38264181a61630f883d
kat test <e_aksenova80@mail.ru>
2023-03-25 14:26:08 +0300

    Main.java style

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class
 Main {
     public static void main(String[] args) {
 
-        int ticket = 1645;  // стоимость билета
+        int ticket = 3248;  // стоимость билета
         int mile = 20;  // миля
-        int bonus; // количество начиленных миль
+        int bonus = 0; // количество начиленных миль
 
-        if (ticket > 0) ;
+        if (ticket > 0)
         {
             bonus = ticket / mile;
         }
